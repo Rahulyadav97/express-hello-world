27ca71e3dd9ba7adde8965152835bbbd97e9cf4a
rahulyadav <rahul.y@spurtreetech.in>
2023-04-16 23:28:21 +0530

    express app

--- app.js
+++ app.js
@@ -1,59 +1,42 @@
 const express = require("express");
+const hbs  = require('express-handlebars');
 const app = express();
-const port = process.env.PORT || 3001;
+const path = require('path');
+app.engine('handlebars', hbs.engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+const port = process.env.PORT || 3000;
+app.use(express.static('./public'))
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get('/', (req, res) => {
+    res.render('home');
+})
+app.get("/quote",function(req,res){
+    var quotes = [
+        {quote:"The main purpose of our life is to be happy,tolerance is the first step to genuine happiness.",author:"Dalai Lama"},
+        {quote:"Time is always moving on, Nothing can stop it. The question is whether we use our time properly or not. We can’t do anything about the past, but what happens in the future depends on what we do now.",author:"me"},
+        {quote:"failure is an oppurtunity to try again",author:"Rahul"},
+        {quote:"Positive mind is peace.",author:"Rahul"},
+        {quote:"Greatness “lies not in being strong but in the right using of strength, he or she is the greatest whose strength carries up the most hearts by the attraction of his own”",author:"Rahul"},
+        {quote:"When given a chance between being right and being kind. Choose kind.",author:"Rahul"},
+        {quote:"Yes you have to leave alone but you got to live together.",author:"Rahul"},
+        {quote:"If you can’t help at least don’t hurt them as you can’t pay  what you hurt them.",author:"Rahul"},
+        {quote:"Life goes by time which never waits and we never know what happen next so always be altruistic, kind, thoughtful and gentle towards others.",author:"Rahul"},
+        {quote:"Yes you have to leave alone but you got to live together.",author:"Rahul"},
+        {quote:"Yes you have to leave alone but you got to live together.",author:"Rahul"},
+        {quote:"Yes you have to leave alone but you got to live together.",author:"Rahul"}
+    ];
+    var randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+    var design = Math.floor(Math.random()*4)
+    res.render("about",{quotes:randomQuote,design:design})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+})
+// app.use((req,res)=>{
+//     res.type("text/plain");
+//     res.status(404);
+//     res.send("page not found");
+// })
 
-
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+app.listen(port,()=>{
+    console.log("listening at 8082")
+});
